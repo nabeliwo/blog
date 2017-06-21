3c4f78388e8b255d35a84f9c10dffdd091f000f9
nabeliwo <hiroki.c.watanabe@gmail.com>
2017-06-22 02:47:16 +0900

    update settings

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # blog.nabeliwo.me
 
-[nabeliwo](https://twitter.com/nabeliwo)'s blog.
+[nabeliwo](https://twitter.com/nabeliwo)'s blog, named nabeliwonote.
 
 [https://blog.nabeliwo.me/](https://blog.nabeliwo.me/)
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-{
-  "input": "./src/css/app.css",
-  "output": "./static/css/bundle.css",
-  "use": [
+module.exports = {
+  input: "./src/css/app.css",
+  output: "./static/css/bundle.css",
+  use: [
     "postcss-import",
     "postcss-custom-properties",
     "postcss-custom-media",
@@ -12,10 +12,10 @@
     "postcss-sorting",
     "csswring"
   ],
-  "autoprefixer": {
-    "browsers": [
+  autoprefixer: {
+    browsers: [
       "last 2 versions",
       "safari >= 7"
     ]
   }
-}
+};
