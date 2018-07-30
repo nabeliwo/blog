336eaae60c5587b61adec97fb1f4deac3e915ce4
nabeliwo <hiroki.c.watanabe@gmail.com>
2018-07-31 08:38:27 +0900

    alert

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,34 @@
-import arrayFrom from './polyfill/array.from';
-import Navi from './navi';
+import arrayFrom from "./polyfill/array.from";
+import Navi from "./navi";
 
 arrayFrom();
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // touch
   function toggleTouch(element) {
-    element.addEventListener('touchstart', () => element.classList.add('is-touch'));
-    element.addEventListener('touchend', () => element.classList.remove('is-touch'));
+    element.addEventListener("touchstart", () =>
+      element.classList.add("is-touch")
+    );
+    element.addEventListener("touchend", () =>
+      element.classList.remove("is-touch")
+    );
   }
-  Array.from(document.querySelectorAll('.js-touch')).forEach(toggleTouch);
-  Array.from(document.querySelectorAll('.c-post a')).forEach(toggleTouch);
+  Array.from(document.querySelectorAll(".js-touch")).forEach(toggleTouch);
+  Array.from(document.querySelectorAll(".c-post a")).forEach(toggleTouch);
 
   // highlight.js
-  const codes = Array.from(document.querySelectorAll('pre code'));
+  const codes = Array.from(document.querySelectorAll("pre code"));
   if (codes.length) {
     codes.forEach(hljs.highlightBlock);
   }
 
   // navi
   new Navi({
-    wrapper: '.js-navi',
-    trigger: '.js-navi-button',
-    bg: '.js-navi-bg',
-    box: '.js-navi-box'
+    wrapper: ".js-navi",
+    trigger: ".js-navi-button",
+    bg: ".js-navi-bg",
+    box: ".js-navi-box"
   });
 });
 
-console.log('hogehoge')
+alert("hogehoge");
