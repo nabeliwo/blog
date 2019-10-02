0125982958b2d82e11b2a2afe9f0b008395258b4
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-10-02 23:15:49 +0900

    :sparkles: Prepare development environment

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:react/recommended', 'prettier/react'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true,
+  },
+  globals: {
+    process: 'readonly',
+  },
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Index = () => <Wrapper>Hello Gatsby!いえあ</Wrapper>
+
+export default Index
+
+const Wrapper = styled.div`
+  color: blue;
+`
