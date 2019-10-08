63bcf8305db1cb12ef28de6a9a40d1d4aeb0cbc1
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-10-09 02:05:58 +0900

    :sparkles: Support favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,6 +122,60 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'ラリルレロ',
+        short_name: 'ラリルレロ',
+        start_url: '/?utm_source=homescreen',
+        background_color: '#fff',
+        theme_color: '#20bffc',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-offline',
     'gatsby-plugin-twitter',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
--- src/libs/ua.js
+++ src/libs/ua.js
@@ -1,4 +1,4 @@
-const ua = window.navigator.userAgent.toLowerCase()
+const ua = ''
 
 export const isIe = ua.indexOf('msie') !== -1 || ua.indexOf('trident') !== -1
 
