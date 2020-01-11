9251b1715a986c756e478c4e44cb5bcbec848359
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-01-11 17:35:33 +0900

    :lipstick: Add 404 style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { size } from '../themes'
 
 import Head from '../components/Head'
 import Layout from '../components/Layout'
@@ -15,9 +18,16 @@ const NotFound = () => (
       ]}
     />
     <Layout>
-      <p>404 Page not found</p>
+      <Text>Sorry, 404 Page not found</Text>
     </Layout>
   </>
 )
 
 export default NotFound
+
+const Text = styled.p`
+  margin-bottom: ${size.space.XXL};
+  padding: 100px 0;
+  font-size: ${size.font.XL};
+  text-align: center;
+`
